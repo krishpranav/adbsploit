ec4434b8bfa98e1c3d4b4c58033e352caef555d3
krishpranav <krisna.pranav@gmail.com>
2021-05-01 11:13:12 +0530

    update commands

---
+++ adbsploit/adbsploit.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env/python3
+# -*- coding: utf-8 -*-
+
+# imports
+import os
+import shutil
+import subprocess
+import sys
+import random
+import adbutils
+
+from colorama import Fore
+from pyfiglet import Figlet
+from rich.console import Console
+from rich.table import Table
+
+# variables and main
+arrow = Fore.RED + " └──>" + Fore.WHITE
+device = 'none'
+
+# main 
+def main():
+    command = input(Fore.WHITE + "adbsploit" + Fore.RED + "(" + device + ")" + Fore.WHITE + " > ")
+    if command == 'help':
+        help()
+        main()
+    elif command == 'devices':
+        devices()
+        main()
+    elif command == 'select':
+        select()
+        main()
+    elif command == 'connect':
+        connect()
+        main()
+    elif command == 'list-forward':
+        list_forward()
+        main()
+    elif command == 'forward':
+        forward()
+        main()
+    elif command == 'wifi':
+        wifi()
+        main()
+    elif command == 'airplane':
+        airplane()
+        main()
+    elif command == 'dumpsys':
+        dumpsys()
+        main()
+    elif command == 'list-apps':
+        list_apps()
+        main()
+    elif command == 'wpa-supplicant':
+        wpa_supplicant()
+        main()
+    elif command == 'start-app':
+        start_app()
+        main()
+    elif command == 'stop-app':
+        stop_app()
+        main()
+    elif command == 'clear-app':
+        clear_app()
+        main()
+    elif command == 'install':
+        install()
+        main()
+    elif command == 'install-remote':
+        install_remote()
+        main()
+    elif command == 'uninstall':
+        uninstall()
+        main()
+    elif command == 'shell':
+        shell()
+        main()
+    elif command == 'shutdown':
+        shutdown()
+        main()
+    elif command == 'reboot':
+        reboot()
+        main()
+    elif command == 'kill-server':
+        kill_server()
+        main()
+    elif command == 'get-folder':
+        get_folder()
+        main()
+    elif command == 'logs':
+        logs()
+        main()
+    elif command == 'show_ip':
+        show_ip()
+        main()
+    elif command == 'battery':
+        battery()
+        main()
+    elif command == 'appinfo':
+        appinfo()
+        main()
+    elif command == 'netstat':
+        netstat()
+        main()
+    elif command == 'sound':
+        sound()
+        main()
+    elif command == 'check-screen':
+        check_screen()
+        main()
+    elif command == 'dump-hierarchy':
+        dump_hierarchy()
+        main()
+    elif command == 'keyevent':
+        keyevent()
+        main()
+    elif command == 'show-keyevents':
+        show_keyevents()
+        main()
+    elif command == 'open-browser':
+        open_browser()
+        main()
+    elif command == 'remove-password':
+        remove_password()
+        main()
+    elif command == 'swipe':
+        swipe()
+        main()
+    elif command == 'screen':
+        screen()
+        main()
+    elif command == 'unlock-screen':
+        unlock_screen()
+        main()
+    elif command == 'lock-screen':
+        lock_screen()
+        main()
+    elif command == 'show-mac':
+        show_macaddress()
+        main()
+    elif command == 'screenshot':
+        screenshot()
+        main()
+    elif command == 'dump-meminfo':
+        dump_meminfo()
+        main()
+    elif command == 'process-list':
+        process_list()
+        main()
+    elif command == 'tcpip':
+        tcpip()
+        main()
+    elif command == 'current-app':
+        current_app()
+        main()
+    elif command == 'extract-contacts':
+        extract_contacts()
+        main()
+    elif command == 'extract-sms':
+        extract_sms()
+        main()
+    elif command == 'delete-sms':
+        delete_sms()
+        main()
+    elif command == 'send-sms':
+        send_sms()
+        main()
+    elif command == 'extract-app':
+        extract_app()
+        main()
+    elif command == 'recovery-mode':
+        recovery_mode()
+        main()
+    elif command == 'device-info':
+        device_info()
+        main()
+    elif command == 'fastboot-mode':
+        fastboot_mode()
+        main()
+    elif command == 'kill-process':
+        kill_process()
+        main()
+    elif command == 'screenrecord':
+        screenrecord()
+        main()
+    elif command == 'remote-control':
+        remote_control()
+        main()
+    elif command == 'backdoor':
+        backdoor()
+        main()
+    elif command == 'clear':
+        clear()
+        main()
+    elif command == 'version':
+        version()
+        main()
+    elif command == 'exit':
+        exit()
+    else:
+        print(arrow + Fore.RED + " That command doesn't exists...")
+        main()
+
+    
